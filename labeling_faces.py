745ab70ceb9250221941b842c527a9673ac5c971
Abhik Sarkar <abhiksark@gmail.com>
2019-02-24 17:40:07 +0530

    Update labeling_faces.py

--- labeling_faces.py
+++ labeling_faces.py
@@ -1,84 +1,161 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
-## Inspiration
-* [OpenFace](https://github.com/cmusatyalab/openface)
-* I refer to the facenet repository of [davidsandberg](https://github.com/davidsandberg/facenet).
-* also, [shanren7](https://github.com/shanren7/real_time_face_recognition) repository was a great help in implementing.
+import tensorflow as tf
+from scipy import misc
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import facenet
+import detect_face
+import os
+from os.path import join as pjoin
+import sys
+import time
+import copy
+import math
+import pickle
+from sklearn.svm import SVC
+from sklearn.externals import joblib
+import re
+from tensorflow.python.platform import gfile
 
-## Dependencies
+def get_model_filenames(model_dir):
+    files = os.listdir(model_dir)
+    meta_files = [s for s in files if s.endswith('.meta')]
+    if len(meta_files)==0:
+        raise ValueError('No meta file found in the model directory (%s)' % model_dir)
+    elif len(meta_files)>1:
+        raise ValueError('There should not be more than one meta file in the model directory (%s)' % model_dir)
+    meta_file = meta_files[0]
+    meta_files = [s for s in files if '.ckpt' in s]
+    max_step = -1
+    for f in files:
+        step_str = re.match(r'(^model-[\w\- ]+.ckpt-(\d+))', f)
+        if step_str is not None and len(step_str.groups())>=2:
+            step = int(step_str.groups()[1])
+            if step > max_step:
+                max_step = step
+                ckpt_file = step_str.groups()[0]
+    return meta_file, ckpt_file
 
-Install the dependencies first for running the code.
-* Tensorflow 1.2.1 - gpu
-* Python 3.5
-* Same as [requirement.txt](https://github.com/davidsandberg/facenet/blob/master/requirements.txt) in [davidsandberg](https://github.com/davidsandberg/facenet) repository.
+def load_model(model):
+    # Check if the model is a model directory (containing a metagraph and a checkpoint file)
+    #  or if it is a protobuf file with a frozen graph
+    model_exp = os.path.expanduser(model)
+    if (os.path.isfile(model_exp)):
+        print('Model filename: %s' % model_exp)
+        with gfile.FastGFile(model_exp,'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+            tf.import_graph_def(graph_def, name='')
+    else:
+        print('Model directory: %s' % model_exp)
+        meta_file, ckpt_file = get_model_filenames(model_exp)
 
-## Pre-trained models
-* Inception_ResNet_v1 CASIA-WebFace-> [20170511-185253](https://drive.google.com/file/d/0B5MzpY9kBtDVOTVnU3NIaUdySFE/edit)
-## Face alignment using MTCNN
-* You need [det1.npy, det2.npy, and det3.npy](https://github.com/davidsandberg/facenet/tree/master/src/align) in the [davidsandberg](https://github.com/davidsandberg/facenet) repository.
-## How to use
-* First, we need align face data. So, if you need to run the script  'raw_faces_to_aligned_faces.py' first, the face data that is aligned in the 'faces/group_photos' folder will be saved.
+        print('Metagraph file: %s' % meta_file)
+        print('Checkpoint file: %s' % ckpt_file)
 
-<img src="https://raw.githubusercontent.com/abhiksark/pictures_cluster_classify/master/gifs/making%20aligned%20faces.gif" width="60%">
+        saver = tf.train.import_meta_graph(os.path.join(model_exp, meta_file))
+        saver.restore(tf.get_default_session(), os.path.join(model_exp, ckpt_file))
 
-* Secord, we will cluster the same photos together by running the clustering_faces.py
- and delete the noise  manually and combine the clustered faces again using combine_cluster_folder.py
+print('Creating networks and loading parameters')
+with tf.Graph().as_default():
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
+    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+    with sess.as_default():
+        pnet, rnet, onet = detect_face.create_mtcnn(sess, './data')
+        minsize = 20  # minimum size of face
+        threshold = [0.6, 0.7, 0.7]  # three steps's threshold
+        #factor =   # scale factor
+        margin = 44
+        frame_interval = 3
+        batch_size = 10
+        image_size = 182
+        input_image_size = 160
+        minsize = 100  # minimum size of face
+        threshold = [0.7, 0.7, 0.7]  # three steps's threshold
+        factor = 0.709  # scale factor
+        margin = 44
+        image_size = 182
 
-<img src="https://raw.githubusercontent.com/abhiksark/pictures_cluster_classify/master/gifs/deleting%20noise.gif" width="60%">
+        HumanNames = os.listdir("./labelled_faces")    #train human name
+        HumanNames.sort()
+        print('Loading feature extraction model')
+        modeldir = './20170511-185253/20170511-185253.pb'
+        load_model(modeldir)
 
+        images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+        embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+        phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+        embedding_size = embeddings.get_shape()[1]
 
-* Third, we need to create our own classifier with the face data we created. First we will rename the folder name with the name of the person. <br/>(In the case of me, I had a high recognition rate when I made 30 pictures for each person.) Your own classifier is a ~.pkl file that loads the previously mentioned pre-trained model ('[20170511-185253.pb](https://drive.google.com/file/d/0B5MzpY9kBtDVOTVnU3NIaUdySFE/edit)') and embeds the face for each person.All of these can be obtained by running 'making_classifier.py'.
+        classifier_filename = './cls/my_classifier.pkl'
+        classifier_filename_exp = os.path.expanduser(classifier_filename)
+        with open(classifier_filename_exp, 'rb') as infile:
+            (model, class_names) = pickle.load(infile)
+            print('load classifier file-> %s' % classifier_filename_exp)
+        video_capture = cv2.VideoCapture(0)
+        c = 0
 
-<img src="https://raw.githubusercontent.com/abhiksark/pictures_cluster_classify/master/gifs/making%20classifier.gif" width="60%">
+        ret = True
+        frame =  cv2.imread('./test.jpg')
 
+        if frame.ndim == 2:
+            frame = facenet.to_rgb(frame)
+        frame = frame[:, :, 0:3]
+        bounding_boxes, _ = detect_face.detect_face(frame, minsize, pnet, rnet, onet, threshold, factor)
+        nrof_faces = bounding_boxes.shape[0]
+        print('Detected_FaceNum: %d' % nrof_faces)
+        det = bounding_boxes[:, 0:4]
+        det_morethanone = det
+        if nrof_faces > 0:
+                    for i in range(nrof_faces):
+                        det = det_morethanone[int(i),:]
+                        img_size = np.asarray(frame.shape)[0:2]
+                        cropped = []
+                        scaled = []
+                        scaled_reshape = []
+                        bb = np.zeros((nrof_faces,4), dtype=np.int32)
+                        emb_array = np.zeros((1, embedding_size))
 
-* Finally, we load our own 'my_classifier.pkl' obtained above and then open the sensor and start recognition by running labeling_faces.py
-<img src="https://raw.githubusercontent.com/abhiksark/pictures_cluster_classify/master/gifs/result.gif" width="60%">
+                        bb[i][0] = det[0]
+                        bb[i][1] = det[1]
+                        bb[i][2] = det[2]
+                        bb[i][3] = det[3]
 
+                        # inner exception
+                        if bb[i][0] <= 0 or bb[i][1] <= 0 or bb[i][2] >= len(frame[0]) or bb[i][3] >= len(frame):
+                            print('face is inner of range!')
+                            continue
 
+                        cropped.append(frame[bb[i][1]:bb[i][3], bb[i][0]:bb[i][2], :])
+                        cropped[0] = facenet.flip(cropped[0], False)
+                        scaled.append(misc.imresize(cropped[0], (image_size, image_size), interp='bilinear'))
+                        scaled[0] = cv2.resize(scaled[0], (input_image_size,input_image_size),
+                                               interpolation=cv2.INTER_CUBIC)
+                        scaled[0] = facenet.prewhiten(scaled[0])
+                        scaled_reshape.append(scaled[0].reshape(-1,input_image_size,input_image_size,3))
+                        feed_dict = {images_placeholder: scaled_reshape[0], phase_train_placeholder: False}
+                        emb_array[0, :] = sess.run(embeddings, feed_dict=feed_dict)
+                        predictions = model.predict_proba(emb_array)
+                        best_class_indices = np.argmax(predictions, axis=1)
+                        best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
+                        cv2.rectangle(frame, (bb[i][0], bb[i][1]), (bb[i][2], bb[i][3]), (0, 255, 0), 2)    #boxing face
+                        #plot result idx under box
+                        text_x = bb[i][0]
+                        text_y = bb[i][3] + 20
+                        print('result: ', best_class_probabilities)
+                        for H_i in HumanNames:
+                            if HumanNames[best_class_indices[0]] == H_i:
+                                result_names = HumanNames[best_class_indices[0]]
+                                cv2.putText(frame, result_names, (text_x, text_y), cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                                            1, (255, 255, 0), thickness=2, lineType=8)
 
-</br> (Note that, look carefully at the paths of files and folders in all .py)
-## Result
-<img src="https://raw.githubusercontent.com/abhiksark/pictures_cluster_classify/master/prediction.jpg" width="60%">
+                    cv2.imwrite('./'+str("prediction")+".jpg", frame)
 
-### Directory Strcuture
+        else:
+                    print('Unable to align')
 
-```bash
-├── 20170511-185253
-│   ├── 20170511-185253.pb 
-│   ├── model-20170511-185253.ckpt-80000.data-00000-of-00001
-│   ├── model-20170511-185253.ckpt-80000.index
-│   └── model-20170511-185253.meta
-├── cls
-│   └── my_classifier.pkl
-├── data
-│   ├── det1.npy
-│   ├── det2.npy
-│   └── det3.npy
-├── faces
-│   └── aligned photos atomatically generated from raw_photos
-├── labelled_faces
-│   └── folder cointaining name of that person
-├── raw_faces
-│   └── Add your group photos here
-├── raw_faces_to_aligned_faces.py
-├── making_classifier.py
-├── ModelManagement.py
-├── labeling_faces.py
-├── detect_face.py
-├── facenet.py
-├── classifying_static_image.py
-├── clustering_faces.py
-├── combine_cluster_folder.py````
-
-## Development
-
-Want to contribute? Great!
-Contact me in LinkedIn!
-
-## License
-
-* <a rel="license" href="https://creativecommons.org/licenses/by-nc-nd/4.0/"> Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License</a>
-
-<a rel="license" href="https://creativecommons.org/licenses/by-nc-nd/4.0/">
-	<img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png" />
-</a>
