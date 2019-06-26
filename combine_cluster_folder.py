e664c6e688af910e346b8767f4acfdde6c18e2fe
Abhik Sarkar <abhiksark@gmail.com>
2019-06-26 10:47:14 +0530

    Update combine_cluster_folder.py

--- combine_cluster_folder.py
+++ combine_cluster_folder.py
@@ -1,6 +1,9 @@
 import os
 
-for i in range(10):
+
+folder_numbers = 10
+
+for i in range(folder_numbers):
     for j in os.listdir("./faces/group_photos/" + str(i)):
     	os.rename("./faces/group_photos/" + str(i)+"/"+str(j) ,"./faces/group_photos/"+str(j))
     os.rmdir("./faces/group_photos/" + str(i))
